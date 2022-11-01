0729c243afeba3ee8ec30d7ddf532d4500d88193
peterk13 <94086841+peterk13@users.noreply.github.com>
2022-11-01 15:41:30 +1000

    Update main.tf

--- automation_lab/main.tf
+++ automation_lab/main.tf
@@ -18,7 +18,7 @@ resource "google_compute_instance" "web" {
   }
   boot_disk {
       initialize_params {
-          image = "debian-cloud/debian-9"
+          image = "debian-cloud/debian-11"
       }
   }
   metadata_startup_script = <<SCRIPT
